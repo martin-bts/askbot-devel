a0a0ad4ead40bb09424e505932cdda487968ebb3
Martin BTS <martinbts@gmx.net>
2019-07-02 10:21:05 +0200

    Add notification type to group_messages app
    * currently we seek to not use group_messages ever, but here it is

--- askbot/deps/group_messaging/context.py
+++ askbot/deps/group_messaging/context.py
@@ -1,7 +1,11 @@
-from askbot.deps.group_messaging.models import get_unread_inbox_counter
+from askbot.deps.group_messaging.models import get_unread_inbox_counter, Message
 
 def group_messaging_context(request):
-    if request.user.is_authenticated:
-        count_record = get_unread_inbox_counter(request.user)
-        return {'group_messaging_unread_inbox_count': count_record.count}
-    return {}
+    if not request.user.is_authenticated:
+        return {}
+    count_record  = get_unread_inbox_counter(request.user)
+    notifications = Message.notifications.get_and_delete_messages(request.user)
+    return {
+        'group_messaging_unread_inbox_count': count_record.count,
+        'group_messaging_notifications': notifications
+        }
