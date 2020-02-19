f5c0ad63c24dc2ec2f9c2a770ef6a91deed1022a
martin-bts <martin-bts@gmx.net>
2020-02-19 15:23:15 +0100

    Move cron_environ to ASKBOT_SITE from /

--- askbot/setup_templates/cron-askbot.sh
+++ askbot/setup_templates/cron-askbot.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-export $(cat /cron_environ | xargs)
-cd ${ASKBOT_SITE:-/askbot-site}
+ASKBOT_SITE=${ASKBOT_SITE:-/askbot-site}
+export $(cat ${ASKBOT_SITE}/cron_environ | xargs)
+cd ${ASKBOT_SITE}
 /usr/local/bin/python manage.py send_email_alerts > /proc/1/fd/1 2>/proc/1/fd/2
